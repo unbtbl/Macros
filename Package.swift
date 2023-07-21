d94dae837d34b5d72a38fe5c0280e03e48d627d7
Robbert Brandsma <i@robbertbrandsma.nl>
2023-07-21 11:55:17 +0200

    Rename package to Macros

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
-    name: "DependenciesMacros",
+    name: "Macros",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
